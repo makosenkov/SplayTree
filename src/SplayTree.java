a2327980cb01ed6b496f03600f719f0dcd478397
makosenkov <makosenkov@gmail.com>
2017-10-05 13:45:58 +0300

    продолжение работы над методами

--- src/SplayTree.java
+++ src/SplayTree.java
@@ -1,7 +1,7 @@
 /**
- *Реализация Splay-дерева (расширяющееся, "выворачивающееся" бинарное дерево)
- * */
-public class SplayTree <Key extends Comparable<Key>, Value>{
+ * Реализация Splay-дерева (расширяющееся, "выворачивающееся" бинарное дерево)
+ */
+public class SplayTree<Key extends Comparable<Key>, Value> {
     private class Node {
         private Node left, right, parent;
         private Key key;
@@ -19,7 +19,15 @@ public class SplayTree <Key extends Comparable<Key>, Value>{
         return root == null;
     }
 
-    private Node rightRotate(Node parent){
+    private boolean isRoot(Node vertex) {
+        return vertex.parent == null;
+    }
+
+    public void split(Node vertex) {
+
+    }
+
+    private Node rightZig(Node parent) {
         Node axis = parent.left; //Берем осевой узел - левый ребенок родителя
         parent.left = axis.right; //Правый ребенок осевого узла становится левым ребенком родителя
         axis.right = parent; //Осевой узел "выплывает" наверх, становясь новым родителем
@@ -27,14 +35,37 @@ public class SplayTree <Key extends Comparable<Key>, Value>{
     }
 
     //Левый поворот аналогично правому, но в зеркальном отражении
-    private Node leftRotate(Node parent){
+    private Node leftZig(Node parent) {
         Node axis = parent.right;
         parent.right = axis.left;
         axis.left = parent;
         return axis;
     }
 
-    private void splay(){}
-
-
+    private Node splay(Node vertex) {
+        if (isRoot(vertex)) return vertex;
+        Node parent = vertex.parent;
+        Node gparent = parent.parent;
+        do {
+            //Zig-Zig
+            if (parent == gparent.left && vertex == parent.left) {
+                rightZig(parent);
+                rightZig(vertex);
+            }
+            if (parent == gparent.right && vertex == parent.right) {
+                leftZig(parent);
+                leftZig(vertex);
+            }
+            //Zig-Zag
+            if (parent == gparent.left && vertex == parent.right) {
+                leftZig(vertex);
+                rightZig(parent);
+            }
+            if (parent == gparent.left && vertex == parent.right) {
+                rightZig(vertex);
+                leftZig(parent);
+            }
+        } while (!isRoot(vertex));
+        return vertex;
+    }
 }
